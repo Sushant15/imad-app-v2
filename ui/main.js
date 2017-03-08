038f67b9281d7e23b4ec73edaccd574ebc55ded3
Sushant15 <sushantgarg15@gmail.com>
2017-03-09 00:08:14 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,3 +1,11 @@
 console.log('Loaded!');
+
+//get  the text of page changed
 var element = document.getElementById('main-text');
 element.innerHTML = 'New Value';
+
+//get the image moving
+var img = document.getElementById('img');
+img.onClick = function() {
+    img.style.marginleft = '100px' ;
+};
