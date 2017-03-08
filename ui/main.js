4687e017510e7307b333d0d5d73b26c8af22fdd7
Sushant15 <sushantgarg15@gmail.com>
2017-03-09 00:09:26 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -5,7 +5,7 @@ var element = document.getElementById('main-text');
 element.innerHTML = 'New Value';
 
 //get the image moving
-var img = document.getElementById('img');
+var img = document.getElementById('madi');
 img.onClick = function() {
     img.style.marginleft = '100px' ;
 };
