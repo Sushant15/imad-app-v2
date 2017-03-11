98fe20777e93746908cb7dd098b467eda65d1e17
Sushant15 <sushantgarg15@gmail.com>
2017-03-11 11:08:33 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -22,3 +22,19 @@ button.onclick = function() {
     request.open('GET', 'http://sushant15.imad.hasura-app.io/counter', true);
     request.send(null);
 };
+
+// Submit name
+var nameInput= document.getElementById('name');
+var name= nameInput.value;
+var submit= document.getElementById('submit_btn');
+submit.onclick = function() {
+    // Make a request to server and send the name
+    //Capture the list of the name and render the list
+    var name= ['Name 1' , 'Name 2','Name 3'];
+    var list= '';
+    for(var i=0;i<name;i++){
+        list+= '<li>' + name[i] + '</li>';
+    }
+    var ul = document.getElementById('namelist');
+    ul.innerHTML = list;
+}
